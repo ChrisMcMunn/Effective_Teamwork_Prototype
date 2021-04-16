f5e0255ba3ee1e4894e615c7785ad81ee36f4bb6
GAD-ChrisMcMunn <63013022+ChrisMcMunn@users.noreply.github.com>
2021-04-16 18:33:32 +0100

    Randomizing Grid Update
    Implemented a script that uses a loop statement to instantiate a grid of randomized sprites that fall under one game object, GridManager.

---
+++ Assets/Prefabs/Grid.cs
@@ -0,0 +1,39 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public int[,] grid;
+    public GameObject GridSpace;
+    private GameObject GridManager;
+    public List<Sprite> piece = new List<Sprite>();
+    void Start()
+    {
+        
+
+        grid = new int[width, height];
+
+        for (int i = 1; i <= height; i++)
+        {
+            for (int j = 1; j <= width; j++)
+                    {
+
+                Vector2 offset = new Vector2(j,i);
+                    GridSpace.GetComponent<SpriteRenderer>().sprite = piece[Random.Range(0,5)];
+                        GameObject GridManager = Instantiate(GridSpace, offset, Quaternion.identity) as GameObject;
+                        GridManager.transform.parent = this.transform;
+                            GridManager.name = ("(" + i + "," + j + ")");
+                       
+                        
+        }
+           
+        }
+        
+    }
+
+        
+}
